6b5eafcde502faf4c42a554645e6276d66d07786
Paul Durrant <paul.durrant@citrix.com>
2013-12-23 16:04:37 +0000

    Update XENVIF and XENNET to fix:
    CA-122017
    CA-123481
    
    Signed-off-by: Paul Durrant <paul.durrant@citrix.com>

--- manifestspecific.py
+++ manifestspecific.py
@@ -30,8 +30,8 @@
 
 build_tar_source_files = {
         "xenbus" : "http://xenbus-build.uk.xensource.com:8080/job/XENBUS.git/13/artifact/xenbus.tar",
-        "xenvif" : "http://xenvif-build.uk.xensource.com:8080/job/XENVIF.git/18/artifact/xenvif.tar",
-        "xennet" : "http://xennet-build.uk.xensource.com:8080/job/XENNET.git/7/artifact/xennet.tar",
+        "xenvif" : "http://xenvif-build.uk.xensource.com:8080/job/XENVIF.git/20/artifact/xenvif.tar",
+        "xennet" : "http://xennet-build.uk.xensource.com:8080/job/XENNET.git/8/artifact/xennet.tar",
         "xeniface" : "http://xeniface-build.uk.xensource.com:8080/job/XENIFACE.git/7/artifact/xeniface.tar",
         "xenvbd" : "http://xenvbd-build.uk.xensource.com:8080/job/XENVBD.git/8/artifact/xenvbd.tar",
         "xenguestagent" : "http://xeniface-build.uk.xensource.com:8080/job/guest%20agent.git/33/artifact/xenguestagent.tar",
