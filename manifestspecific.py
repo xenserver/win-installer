070aee86c01a72fb10034b2a3dbc856d246cbefc
Paul Durrant <paul.durrant@citrix.com>
2014-08-08 10:20:07 +0100

    [XOP-577] XenVif #48
    Treat a missing Aliases value as an empty one — paul.durrant
    
    Signed-off-by: Paul Durrant <paul.durrant@citrix.com>

--- manifestspecific.py
+++ manifestspecific.py
@@ -33,7 +33,7 @@ localserver = r'\\camos.uk.xensource.com\build\windowsbuilds\WindowsBuilds'
 
 build_tar_source_files = {
        "xenbus" : r'standard-lcm\10\xenbus-7-2-0-47.tar',
-       "xenvif" : r'xenvif.git\46\xenvif.tar',
+       "xenvif" : r'xenvif.git\48\xenvif.tar',
        "xennet" : r'standard-lcm\13\xennet-7-2-0-14.tar',
        "xeniface" : r'standard-lcm\12\xeniface-7-2-0-14.tar',
        "xenvbd" : r'standard-lcm\14\xenvbd-7-2-0-40.tar',
