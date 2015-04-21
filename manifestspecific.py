2dc89704fbe9708c920a2f98aea0e799d2f67b4e
Paul Durrant <paul.durrant@citrix.com>
2015-04-21 10:14:35 +0100

    Update XENVIF to tip
    Signed-off-by: Paul Durrant <paul.durrant@citrix.com>

--- manifestspecific.py
+++ manifestspecific.py
@@ -33,7 +33,7 @@ localserver = r'\\camos.uk.xensource.com\build\windowsbuilds\WindowsBuilds'
 
 build_tar_source_files = {
        "xenbus" : r'xenbus-patchq.git\31\xenbus.tar',
-       "xenvif" : r'xenvif-patchq.git\12\xenvif.tar',
+       "xenvif" : r'xenvif-patchq.git\13\xenvif.tar',
        "xennet" : r'xennet-patchq.git\11\xennet.tar',
        "xeniface" : r'xeniface-patchq.git\5\xeniface.tar',
        "xenvbd" : r'xenvbd-patchq.git\13\xenvbd.tar',
