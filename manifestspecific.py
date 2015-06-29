c25746e96b0ba75612ada84dbd7413f09e717bdb
Owen Smith <owen.smith@citrix.com>
2015-06-29 12:44:04 +0100

    XenVbd #96
    Signed-off-by: Owen Smith <owen.smith@citrix.com>

--- manifestspecific.py
+++ manifestspecific.py
@@ -36,7 +36,7 @@ build_tar_source_files = {
        "xenvif" : r'xenvif-patchq.git\14\xenvif.tar',
        "xennet" : r'xennet-patchq.git\11\xennet.tar',
        "xeniface" : r'xeniface-patchq.git\5\xeniface.tar',
-       "xenvbd" : r'xenvbd-patchq.git\95\xenvbd.tar',
+       "xenvbd" : r'xenvbd-patchq.git\96\xenvbd.tar',
        "xenguestagent" : r'xenguestagent.git\131\xenguestagent.tar',
        "xenvss" : r'standard-lcm\16\xenvss-7.tar',
 } 
