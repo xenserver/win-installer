fc38098b946b7ce19fa2d616abcdf0859969da21
Ben Chalmers <Ben.Chalmers@citrix.com>
2015-08-03 10:03:02 +0100

    CP-13459 : introduce xenprep into installer build
    Currently a stub version of the xenprep utility which does nothing useful
    It is included in installer/builds (and thus in the output collected
    in the installer tar file, but not used beyond that point)
    
    The intent is that all useful functionality will go into XenPrepSupport.cs,
    which may also be included by other tasks that require the same functionality
    (in particular the installwizard)

---
+++ src/xenprep/Progress.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Xenprep
+{
+    public partial class Progress : Form
+    {
+        public Progress()
+        {
+            InitializeComponent();
+            progressBar.Maximum = 100;
+            progressBar.Minimum = 0;
+        }
+    }
+}
---
+++ src/xenprep/XenPrepSupport.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Xenprep
+{
+    class XenPrepSupport
+    {
+        public static void LockCD() {}
+        public static void SetRestorePoint() {}
+        public static void StoreNetworkSettings() {}
+        public static void RemovePVDriversFromFilters() {}
+        public static void DontBootStartPVDrivers() {}
+        public static void UninstallMSIs() {}
+        public static void UninstallXenLegacy() {}        
+        public static void CleanUpPVDrivers() {}
+        public static void InstallGuestAgent() {}
+        public static void UnlockCD() { }
+    }
+}
