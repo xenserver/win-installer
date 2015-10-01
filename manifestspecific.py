189f20ee85964d9d3c1ae97eac9c20088518c706
Paul Durrant <paul.durrant@citrix.com>
2015-10-01 16:38:26 +0100

    Correct xenbus build number
    Signed-off-by: Paul Durrant <paul.durrant@citrix.com>

--- manifestspecific.py
+++ manifestspecific.py
@@ -33,7 +33,7 @@ localserver = r'\\camos.uk.xensource.com\build\windowsbuilds\WindowsBuilds'
 
 build_tar_source_files = {
        "xenguestagent" : r'xenguestagent.git\143\xenguestagent.tar',
-       "xenbus" : r'xenbus-patchq.git\49\xenbus.tar',
+       "xenbus" : r'xenbus-patchq.git\47\xenbus.tar',
        "xenvif" : r'xenvif-patchq.git\41\xenvif.tar',
        "xennet" : r'xennet-patchq.git\24\xennet.tar',
        "xeniface" : r'xeniface-patchq.git\13\xeniface.tar',
