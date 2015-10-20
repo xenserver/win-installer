b6a356ff41407f5ef2cad108c0e73eed4aa96151
Ben Chalmers <Ben.Chalmers@citrix.com>
2015-10-20 09:09:59 +0100

    "xeniface #15"

--- manifestspecific.py
+++ manifestspecific.py
@@ -36,7 +36,7 @@ build_tar_source_files = {
        "xenbus" : r'xenbus-patchq.git\47\xenbus.tar',
        "xenvif" : r'xenvif-patchq.git\43\xenvif.tar',
        "xennet" : r'xennet-patchq.git\26\xennet.tar',
-       "xeniface" : r'xeniface-patchq.git\13\xeniface.tar',
+       "xeniface" : r'xeniface-patchq.git\15\xeniface.tar',
        "xenvbd" : r'xenvbd-patchq.git\111\xenvbd.tar',
        "xenvss" : r'standard-lcm\16\xenvss-7.tar',
 }
