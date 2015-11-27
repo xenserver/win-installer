29f7429a74cbf06a97d3799852f81dcf94365017
Kostas Ladopoulos <konstantinos.ladopoulos@citrix.com>
2015-11-27 15:42:08 +0000

    Added PInvoke.Win32ErrorMessage class
    More elegant way to GetLastWin32Error
    
    Signed-off-by: Kostas Ladopoulos <konstantinos.ladopoulos@citrix.com>

---
+++ src/InstallAgent/PInvoke/Win32ErrorMessage.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel;
+using System.Runtime.InteropServices;
+
+namespace PInvoke
+{
+    public static class Win32ErrorMessage
+    {
+        private static string message;
+
+        public static string GetLast()
+        {
+            return message;
+        }
+
+        public static void FindLast(string win32FuncName)
+        {
+            message =
+                win32FuncName + " failed: " +
+                new Win32Exception(
+                    Marshal.GetLastWin32Error()
+                ).Message;
+        }
+    }
+}
