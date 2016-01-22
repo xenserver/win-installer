89e64aa85396e5453eb13a758006bf5379ce3aca
Paul Durrant <paul.durrant@citrix.com>
2016-01-22 13:20:11 +0000

    Update XENVBD
    Signed-off-by: Paul Durrant <paul.durrant@citrix.com>

--- manifestspecific.py
+++ manifestspecific.py
@@ -37,7 +37,7 @@ build_tar_source_files = {
        "xenvif" : r'xenvif-patchq.git\56\xenvif.tar',
        "xennet" : r'xennet-patchq.git\35\xennet.tar',
        "xeniface" : r'xeniface-patchq.git\25\xeniface.tar',
-       "xenvbd" : r'xenvbd-patchq.git\121\xenvbd.tar',
+       "xenvbd" : r'xenvbd-patchq.git\122\xenvbd.tar',
        "xenvss" : r'standard-lcm\16\xenvss-7.tar',
 }
 
