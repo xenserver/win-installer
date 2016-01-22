a3c7a92a993897a75d944e4ba9be25a71855aade
Paul Durrant <paul.durrant@citrix.com>
2016-01-22 17:39:38 +0000

    Update all drivers
    Signed-off-by: Paul Durrant <paul.durrant@citrix.com>

--- manifestspecific.py
+++ manifestspecific.py
@@ -33,11 +33,11 @@ localserver = r'\\camos.uk.xensource.com\build\windowsbuilds\WindowsBuilds'
 
 build_tar_source_files = {
        "xenguestagent" : r'xenguestagent.git\157\xenguestagent.tar',
-       "xenbus" : r'xenbus-patchq.git\60\xenbus.tar',
-       "xenvif" : r'xenvif-patchq.git\56\xenvif.tar',
-       "xennet" : r'xennet-patchq.git\35\xennet.tar',
-       "xeniface" : r'xeniface-patchq.git\25\xeniface.tar',
-       "xenvbd" : r'xenvbd-patchq.git\122\xenvbd.tar',
+       "xenbus" : r'xenbus-patchq.git\63\xenbus.tar',
+       "xenvif" : r'xenvif-patchq.git\58\xenvif.tar',
+       "xennet" : r'xennet-patchq.git\37\xennet.tar',
+       "xeniface" : r'xeniface-patchq.git\26\xeniface.tar',
+       "xenvbd" : r'xenvbd-patchq.git\124\xenvbd.tar',
        "xenvss" : r'standard-lcm\16\xenvss-7.tar',
 }
 
