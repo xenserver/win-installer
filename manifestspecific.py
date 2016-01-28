c7d28e69cbcc7367b843641777816c6fae42440e
Paul Durrant <paul.durrant@citrix.com>
2016-01-28 11:31:39 +0000

    Update XENNET
    Signed-off-by: Paul Durrant <paul.durrant@citrix.com>

--- manifestspecific.py
+++ manifestspecific.py
@@ -35,7 +35,7 @@ build_tar_source_files = {
        "xenguestagent" : r'xenguestagent.git\168\xenguestagent.tar',
        "xenbus" : r'xenbus-patchq.git\63\xenbus.tar',
        "xenvif" : r'xenvif-patchq.git\59\xenvif.tar',
-       "xennet" : r'xennet-patchq.git\39\xennet.tar',
+       "xennet" : r'xennet-patchq.git\40\xennet.tar',
        "xeniface" : r'xeniface-patchq.git\26\xeniface.tar',
        "xenvbd" : r'xenvbd-patchq.git\124\xenvbd.tar',
        "xenvss" : r'standard-lcm\16\xenvss-7.tar',
