aa82b2f12f66cac4fb6e27c1954b3e70857f222c
Ben Chalmers <Ben.Chalmers@citrix.com>
2016-02-11 14:18:36 +0000

    CP-14538 : Disable PV drivers on uninstall

---
+++ src/Uninstall/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+using HelperFunctions;
+using PVDriversRemoval;
+
+namespace Uninstall
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Helpers.ChangeServiceStartMode(
+                "xenbus", 
+                Helpers.ExpandedServiceStartMode.Disabled
+            );
+            PVDriversPurge.RemovePVDriversFromFilters();
+        }
+    }
+}
