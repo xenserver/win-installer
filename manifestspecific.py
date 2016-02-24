8b0afff3faea8aed6a3ce91d16b208a6947e8ed3
Paul Durrant <paul.durrant@citrix.com>
2016-02-24 11:00:01 +0000

    Update XENNET
    This version contains the fix for CA-200425
    
    Signed-off-by: Paul Durrant <paul.durrant@citrix.com>

--- manifestspecific.py
+++ manifestspecific.py
@@ -35,7 +35,7 @@ build_tar_source_files = {
        "xenguestagent" : r'xenguestagent.git\175\xenguestagent.tar',
        "xenbus" : r'xenbus-patchq.git\63\xenbus.tar',
        "xenvif" : r'xenvif-patchq.git\60\xenvif.tar',
-       "xennet" : r'xennet-patchq.git\41\xennet.tar',
+       "xennet" : r'xennet-patchq.git\42\xennet.tar',
        "xeniface" : r'xeniface-patchq.git\26\xeniface.tar',
        "xenvbd" : r'xenvbd-patchq.git\124\xenvbd.tar',
        "xenvss" : r'xenvss.git\15\xenvss.tar',
