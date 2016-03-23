f7ce8651ba496dd814a68bd380f20dc6707c618f
Paul Durrant <paul.durrant@citrix.com>
2016-03-23 14:58:58 +0000

    Update XENVIF
    Signed-off-by: Paul Durrant <paul.durrant@citrix.com>

--- manifestspecific.py
+++ manifestspecific.py
@@ -34,7 +34,7 @@ localserver = r'\\camos.uk.xensource.com\build\windowsbuilds\WindowsBuilds'
 build_tar_source_files = {
        "xenguestagent" : r'xenguestagent.git\181\xenguestagent.tar',
        "xenbus" : r'xenbus-patchq.git\63\xenbus.tar',
-       "xenvif" : r'xenvif-patchq.git\61\xenvif.tar',
+       "xenvif" : r'xenvif-patchq.git\62\xenvif.tar',
        "xennet" : r'xennet-patchq.git\42\xennet.tar',
        "xeniface" : r'xeniface-patchq.git\26\xeniface.tar',
        "xenvbd" : r'xenvbd-patchq.git\130\xenvbd.tar',
