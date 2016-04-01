cec1af75119984662dae93237ff779826227e171
Owen Smith <owen.smith@citrix.com>
2016-04-01 14:04:19 +0100

    Add WHQLed drivers * xenbus (8.1.0.63) * xeniface (8.1.0.26) * xenvif (8.1.0.62) * xennet (8.1.0.42) * xenvbd (8.1.0.130)
    Signed-off-by: Owen Smith <owen.smith@citrix.com>

--- manifestspecific.py
+++ manifestspecific.py
@@ -33,12 +33,12 @@ localserver = r'\\camos.uk.xensource.com\build\windowsbuilds\WindowsBuilds'
 
 build_tar_source_files = {
        "xenguestagent" : r'xenguestagent.git\183\xenguestagent.tar',
-       "xenbus" : r'xenbus-patchq.git\63\xenbus.tar',
-       "xenvif" : r'xenvif-patchq.git\62\xenvif.tar',
-       "xennet" : r'xennet-patchq.git\42\xennet.tar',
-       "xeniface" : r'xeniface-patchq.git\26\xeniface.tar',
-       "xenvbd" : r'xenvbd-patchq.git\130\xenvbd.tar',
+       "xenbus" : r'xenbus-patchq.git.whql\63-all\xenbus.tar',
+       "xenvif" : r'xenvif-patchq.git.whql\62-all\xenvif.tar',
+       "xennet" : r'xennet-patchq.git.whql\42-all\xennet.tar',
+       "xeniface" : r'xeniface-patchq.git.whql\26-all\xeniface.tar',
+       "xenvbd" : r'xenvbd-patchq.git.whql\130-all\xenvbd.tar',
        "xenvss" : r'xenvss.git\15\xenvss.tar',
 }
 
-all_drivers_signed = False
+all_drivers_signed = True
