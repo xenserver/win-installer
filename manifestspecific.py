33343159ef2689656f7172a07f3a1d0c9f6dddf3
Ben Chalmers <Ben.Chalmers@citrix.com>
2016-07-20 12:29:29 +0100

    XGA 202
    CP-17860 : Only tell MSIs to install drivers on update if we are allowed
    CP-17863 : Change updates url to updates.v2.tsv

--- manifestspecific.py
+++ manifestspecific.py
@@ -32,7 +32,7 @@ secureserver = r'\\10.80.13.10\distfiles\distfiles\WindowsBuilds'
 localserver = r'\\filer.do.citrite.net\build\windowsbuilds\WindowsBuilds'
 
 build_tar_source_files = {
-       "xenguestagent" : r'xenguestagent.git\201\xenguestagent.tar',
+       "xenguestagent" : r'xenguestagent.git\202\xenguestagent.tar',
        "xenbus" : r'xenbus-patchq.git\65\xenbus.tar',
        "xenvif" : r'xenvif-patchq.git\63\xenvif.tar',
        "xennet" : r'xennet-patchq.git\43\xennet.tar',
