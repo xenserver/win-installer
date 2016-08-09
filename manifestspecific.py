263bb84d326c4d73396c13452097ae0771163a3b
Ben Chalmers <Ben.Chalmers@citrix.com>
2016-08-09 09:35:56 +0100

    Add guestagent (secret) 206

--- manifestspecific.py
+++ manifestspecific.py
@@ -32,7 +32,7 @@ secureserver = r'\\10.80.13.10\distfiles\distfiles\WindowsBuilds'
 localserver = r'\\filer.do.citrite.net\build\windowsbuilds\WindowsBuilds'
 
 build_tar_source_files = {
-       "xenguestagent" : r'xenguestagent.git\201\xenguestagent.tar',
+       "xenguestagent" : r'xenguestagentsecret.git\206\xenguestagent.tar',
        "xenbus" : r'xenbus-patchq.git\69\xenbus.tar',
        "xenvif" : r'xenvif-patchq.git\67\xenvif.tar',
        "xennet" : r'xennet-patchq.git\45\xennet.tar',
