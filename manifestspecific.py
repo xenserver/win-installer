ffc86b7fd5ef152d5a00bbdfcab7b69cb26be180
Paul Durrant <paul.durrant@citrix.com>
2016-08-09 17:34:48 +0100

    Update all drivers except XENIFACE
    Signed-off-by: Paul Durrant <paul.durrant@citrix.com>

--- manifestspecific.py
+++ manifestspecific.py
@@ -33,11 +33,11 @@ localserver = r'\\filer.do.citrite.net\build\windowsbuilds\WindowsBuilds'
 
 build_tar_source_files = {
        "xenguestagent" : r'xenguestagentsecret.git\206\xenguestagent.tar',
-       "xenbus" : r'xenbus-patchq.git\69\xenbus.tar',
-       "xenvif" : r'xenvif-patchq.git\67\xenvif.tar',
-       "xennet" : r'xennet-patchq.git\45\xennet.tar',
+       "xenbus" : r'xenbus-patchq.git\70\xenbus.tar',
+       "xenvif" : r'xenvif-patchq.git\68\xenvif.tar',
+       "xennet" : r'xennet-patchq.git\46\xennet.tar',
        "xeniface" : r'xeniface-patchq.git\43\xeniface.tar',
-       "xenvbd" : r'xenvbd-patchq.git\135\xenvbd.tar',
+       "xenvbd" : r'xenvbd-patchq.git\137\xenvbd.tar',
        "xenvss" : r'xenvss.git\15\xenvss.tar',
 }
 
