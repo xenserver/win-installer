f2b1a2329581e159e8d386636f2fe2a1de5bff4a
Paul Durrant <paul.durrant@citrix.com>
2016-10-03 15:42:56 +0100

    Update XENVIF and XENVBD
    Signed-off-by: Paul Durrant <paul.durrant@citrix.com>

--- manifestspecific.py
+++ manifestspecific.py
@@ -34,10 +34,10 @@ localserver = r'\\filer.do.citrite.net\build\windowsbuilds\WindowsBuilds'
 build_tar_source_files = {
        "xenguestagent" : r'xenguestagentsecret.git\206\xenguestagent.tar',
        "xenbus" : r'xenbus-patchq.git\78\xenbus.tar',
-       "xenvif" : r'xenvif-patchq.git\82\xenvif.tar',
+       "xenvif" : r'xenvif-patchq.git\83\xenvif.tar',
        "xennet" : r'xennet-patchq.git\53\xennet.tar',
        "xeniface" : r'xeniface-patchq.git\45\xeniface.tar',
-       "xenvbd" : r'xenvbd-patchq.git\143\xenvbd.tar',
+       "xenvbd" : r'xenvbd-patchq.git\144\xenvbd.tar',
        "xenvss" : r'xenvss.git\15\xenvss.tar',
 }
 
