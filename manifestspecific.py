1b567ebee1bb478b2ed5cd8d91afd358413949f7
Paul Durrant <paul.durrant@citrix.com>
2016-11-07 13:08:33 +0000

    Update XENVIF
    Signed-off-by: Paul Durrant <paul.durrant@citrix.com>

--- manifestspecific.py
+++ manifestspecific.py
@@ -33,7 +33,7 @@ artifactory='https://repo.citrite.net:443/xenserver/transformer/'
 build_tar_source_files = {
        "xenguestagent" : r'win-xenguestagent/master/win-xenguestagent-212/xenguestagent.tar',
        "xenbus" : r'win-xenbus/patchq/win-xenbus-82/xenbus.tar',
-       "xenvif" : r'win-xenvif/patchq/win-xenvif-96/xenvif.tar',
+       "xenvif" : r'win-xenvif/patchq/win-xenvif-97/xenvif.tar',
        "xennet" : r'win-xennet/patchq/win-xennet-61/xennet.tar',
        "xeniface" : r'win-xeniface/patchq/win-xeniface-51/xeniface.tar',
        "xenvbd" : r'win-xenvbd/patchq/win-xenvbd-148/xenvbd.tar',
