7b4b27f58e96372ab7909b319657ba3ceb79c02f
Lin Liu <lin.liu@citrix.com>
2017-07-04 10:34:08 +0000

    bumpup xenbus version
    Signed-off-by: Lin Liu <lin.liu@citrix.com>

--- manifestspecific.py
+++ manifestspecific.py
@@ -32,7 +32,7 @@ artifactory='https://repo.citrite.net:443/xs-local-build/'
 
 build_tar_source_files = {
        "xenguestagent" : r'win-xenguestagent/master/win-xenguestagent-219/xenguestagent.tar',
-       "xenbus" : r'win-xenbus/patchq-8.2/win-xenbus-106/xenbus.tar',
+       "xenbus" : r'win-xenbus/patchq-8.2/win-xenbus-107/xenbus.tar',
        "xenvif" : r'win-xenvif/patchq/win-xenvif-103/xenvif.signed.tar',
        "xennet" : r'win-xennet/patchq/win-xennet-64/xennet.signed.tar',
        "xeniface" : r'win-xeniface/8.2/win-xeniface-102/xeniface.tar',
