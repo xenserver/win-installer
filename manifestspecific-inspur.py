4687173badecb91d478c68e68c06aa410d23889a
Lin Liu <lin.liu@citrix.com>
2017-11-08 06:41:42 +0000

    CA-272196: [HFPHokulea] Windows guest driver package failed signature verification
    Signed-off-by: Lin Liu <lin.liu@citrix.com>

--- manifestspecific-inspur.py
+++ manifestspecific-inspur.py
@@ -41,6 +41,11 @@ build_tar_source_files = {
 }
 
 signed_drivers = {
+        "xenbus": r"win-xenbus/patchq-8.2/win-xenbus-113/xenbus.inspur.signed.tar",
+        "xenvif": r"win-xenvif/patchq/win-xenvif-152/xenvif.inspur.signed.tar",
+        "xennet": r"win-xennet/patchq/win-xennet-64/xennet.inspur.signed.tar",
+        "xeniface": r"win-xeniface/patchq/win-xeniface-102/xeniface.inspur.signed.tar",
+        "xenvbd": r"win-xenvbd/patchq/win-xenvbd-158/xenvbd.inspur.signed.tar", 
 }
 
 
