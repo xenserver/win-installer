4016f9a9de41a09973f9011c13111c0b0021fd95
Kostas Ladopoulos <konstantinos.ladopoulos@citrix.com>
2017-11-29 11:18:12 +0000

    CA-257181: Update XenBus to #117
    Contains "Reboot request keys should be volatile" commit
    
    Signed-off-by: Kostas Ladopoulos <konstantinos.ladopoulos@citrix.com>

--- manifestspecific.py
+++ manifestspecific.py
@@ -32,7 +32,7 @@ artifactory='https://repo.citrite.net:443/xs-local-build/'
 
 build_tar_source_files = {
        "xenguestagent" : r'win-xenguestagent/master/win-xenguestagent-219/xenguestagent.tar',
-       "xenbus" : r'win-xenbus/patchq-8.2/win-xenbus-113/xenbus.tar',
+       "xenbus" : r'win-xenbus/patchq-8.2/win-xenbus-117/xenbus.tar',
        "xenvif" : r'win-xenvif/patchq/win-xenvif-152/xenvif.tar',
        "xennet" : r'win-xennet/patchq/win-xennet-102/xennet.tar',
        "xeniface" : r'win-xeniface/8.2/win-xeniface-103/xeniface.tar',
